3fc8de36ed1f188c9d6d43b0d9a82e0fdc9ff187
Kevin Mathew <kemathew@cisco.com>
2019-01-26 23:59:15 -0800

    added lambda functions for few endpoints

---
+++ authors.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"blogging/models"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var errorLogger = log.New(os.Stderr, "ERROR ", log.Llongfile)
+
+func router(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	switch req.HTTPMethod {
+	case "GET":
+		return show(req)
+	case "POST":
+		return create(req)
+	default:
+		return clientError(http.StatusMethodNotAllowed)
+	}
+}
+
+func show(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	email := req.QueryStringParameters[models.Email]
+	dispName := req.QueryStringParameters[models.DispName]
+
+	author, err := getAuthor(email, dispName)
+	if err != nil {
+		return serverError(err)
+	}
+	if author == nil {
+		return clientError(http.StatusNotFound)
+	}
+
+	js, err := json.Marshal(author)
+	if err != nil {
+		return serverError(err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(js),
+	}, nil
+}
+
+func create(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if req.Headers["Content-Type"] != "application/json" {
+		return clientError(http.StatusNotAcceptable)
+	}
+
+	author := new(models.Author)
+	err := json.Unmarshal([]byte(req.Body), author)
+	if err != nil {
+		return clientError(http.StatusUnprocessableEntity)
+	}
+
+	if author.Email == "" || author.DisplayName == "" {
+		return clientError(http.StatusBadRequest)
+	}
+
+	err = registerAuthor(author.Name, author.DisplayName, author.Email)
+	if err != nil {
+		return serverError(err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 201,
+		Headers:    map[string]string{"Location": fmt.Sprintf("/author?email=%s", author.Email)},
+	}, nil
+}
+
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	errorLogger.Println(err.Error())
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       http.StatusText(http.StatusInternalServerError),
+	}, nil
+}
+
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+	}, nil
+}
+
+func main() {
+	lambda.Start(router)
+}
